80573fb55947f4fff6bbea2390ec218eb5a61f18
AdezAtl <adezatl4336@gmail.com>
2026-09-02 06:45:13 +0100

    added an adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://abdullah-can-code.vercel.app',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
