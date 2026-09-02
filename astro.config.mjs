66fa70720906decb621e60cb2ba9e2544c8cd1a2
AdezAtl <adezatl4336@gmail.com>
2026-09-02 10:02:26 +0100

    finally fixed the shii

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
 
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://abdullah-can-code.vercel.app',
-  adapter: vercel(),
   vite: {
-    plugins: [tailwindcss()],
-  },
+    plugins: [tailwindcss()]
+  }
 });
